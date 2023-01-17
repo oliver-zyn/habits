48c4dda6b811e284737b9b09c646e17c65f7a2f9
oliver-zyn <olivermayer15@gmail.com>
2023-01-16 23:17:45 -0300

    commit inicial - aula 01

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit";
+
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit />
+      <Habit />
+    </div>
+  )
+}
